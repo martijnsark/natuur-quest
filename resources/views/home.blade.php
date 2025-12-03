f213a8e554fc2fbd32da03a19aa5727573b6924a
Thomasplie <1039350@hr.nl>
2025-12-03 13:40:45 +0100

    Setting up homepage
    Set-up of homepage

---
+++ resources/views/home.blade.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Natuur Quest</title>
+    @vite('resources/css/app.css')
+</head>
+<body class="flex flex-col min-h-screen bg-green-900">
+
+    <header class="bg-lime-500">
+        <p>BrothaBrotha,Brothaaa</p>
+    </header>
+
+    <main class="flex flex-1 justify-center items-center p-4">
+        <img src="https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/810.png"
+        alt="Monke" class="-rotate-45"
+        >
+    </main>
+
+    <footer class="w-full max-h-[10vh] overflow-hidden flex">
+        <div class="w-full h-full bg-blue-500 text-white flex items-center justify-center">
+            Test Footer Content
+        </div>
+        <div class="w-full h-72 bg-blue-500 text-white flex items-center justify-center">
+            Test Footer Content
+        </div>
+
+    </footer>
+
+</body>
+</html>
