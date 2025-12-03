d9bf3d391aa39b2d15e3cccbef8c2a8012bd5d17
eline <1096534@hr.nl>
2025-12-03 13:05:17 +0100

    ADD html landmarks

--- resources/views/challenges/show.blade.php
+++ resources/views/challenges/show.blade.php
@@ -1,28 +1,36 @@
 {{--<x-app-layout>--}}
-<h1>{{$challenge->title}}</h1>
-<p>De opdracht is om een waterbak in je natuurgebied te vullen. De waterbak
-    die jij moet gaan vullen
-    is <span class="font-semibold">{{$challenge->water_trough}}</span>.</p>
-<p>Ga naar deze waterbak opzoek en daar vind je de vraag die je moet beantwoorden. Vul je antwoord hieronder in.</p>
+<header>
+    <h1>{{$challenge->title}}</h1>
+</header>
+<main>
+    <section aria-label="Uitleg opdracht">
+        <p>De opdracht is om een waterbak in je natuurgebied te vullen. De waterbak
+            die jij moet gaan vullen
+            is <span class="font-semibold">{{$challenge->water_trough}}</span>.</p>
+        <p>Ga naar deze waterbak opzoek en daar vind je de vraag die je moet beantwoorden. Vul je antwoord hieronder
+            in.</p>
+    </section>
+    {{--antwoorden van de challenge--}}
+    <section aria-label="Antwoorden voor de opdracht">
+        <form method="POST" action="{{--route to new page--}}">
+            @csrf
+            <fieldset>
+                <legend>Kies uit</legend>
 
-{{--antwoorden van de challenge--}}
-<form method="POST" action="{{--route to new page--}}">
-    @csrf
-    <fieldset>
-        <legend>Kies uit</legend>
 
+                <input type="radio" name="option_{{$challenge->id}}" id="a" value="a">
+                <label for="a">a</label>
 
-        <input type="radio" name="option_{{$challenge->id}}" id="a" value="a">
-        <label for="a">a</label>
+                <input type="radio" name="option_{{$challenge->id}}" id="b" value="b">
+                <label for="b">b</label>
 
-        <input type="radio" name="option_{{$challenge->id}}" id="b" value="b">
-        <label for="b">b</label>
+                <input type="radio" name="option_{{$challenge->id}}" id="c" value="c">
+                <label for="c">c</label>
 
-        <input type="radio" name="option_{{$challenge->id}}" id="c" value="c">
-        <label for="c">c</label>
-
-    </fieldset>
-    {{--submit button--}}
-</form>
+            </fieldset>
+            {{--submit button--}}
+        </form>
+    </section>
+</main>
 {{--</x-app-layout>--}}
 
