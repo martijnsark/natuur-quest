96403b405c673f000a739b075d3339d36ec37d11
Martijn <m.van.der.willigen@sintlucasedu.nl>
2025-12-03 14:57:41 +0100

    Revert "failed livewire test (missing files)"
    This reverts commit f4ff685bfd3a0c922f545840a5bc5f12810f3f2a.

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,6 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
-use App\Livewire\Chat;
-use Livewire\Volt\Volt;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -17,8 +15,6 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
-    Route::get("chat", Chat::class)->name("chat");
 });
 
 require __DIR__.'/auth.php';
