0b095749f46a829ae3355ccf0e15f0cd6f2b4349
Your Name <you@example.com>
2025-12-08 15:25:06 +0100

    Layout footernav v1.1.1 (Href changes en temp routes)

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -15,22 +15,9 @@
 <footer class="fixed bottom-0 left-0 w-full text-white">
     <nav class="h-20 flex items-end justify-between gap-px ">
 
-        <a href="/audio"
-           class="bg-[#006B56] flex-1 h-16 border border-white/30 flex items-center justify-center">
-            <span class="text-xl">🎧</span>
-        </a>
-
-        <a href="/map"
-           class="bg-[#006B56] flex-1 h-16 border border-white/30 flex items-center justify-center">
-            <span class="text-xl">🗺️</span>
-        </a>
-
-        {{-- 3: Home (actief, hoger en afgerond) --}}
         <a href="/welcome"
-           class="flex-1 h-20 bg-[#006B56] border border-white/30
-          rounded-t-3xl flex items-center justify-center
-          -translate-y-[px] shadow-[0_-2px_0_rgba(255,255,255,0.6)]">
-            <span class="text-2xl">🏠</span>
+           class="bg-[#006B56] flex-1 h-16 border border-white/30 flex items-center justify-center">
+            <span class="text-xl">🏠</span>
         </a>
 
         <a href="/friends"
@@ -38,9 +25,21 @@
             <span class="text-xl">👥</span>
         </a>
 
+        <a href="/challenge"
+           class="flex-1 h-20 bg-[#006B56] border border-white/30
+          rounded-t-3xl flex items-center justify-center
+          -translate-y-[px] shadow-[0_-2px_0_rgba(255,255,255,0.6)]">
+            <span class="text-2xl">Play</span>
+        </a>
+
         <a href="/shop"
            class="bg-[#006B56] flex-1 h-16 border border-white/30 flex items-center justify-center">
             <span class="text-xl">🏪</span>
+        </a>
+
+        <a href="/Profile"
+           class="bg-[#006B56] flex-1 h-16 border border-white/30 flex items-center justify-center">
+            <span class="text-xl">>.<</span>
         </a>
 
     </nav>
