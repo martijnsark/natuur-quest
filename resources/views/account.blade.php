c8c733ab70531b2a39a7a68de7fbb6fc7c362dbe
Martijn <m.van.der.willigen@sintlucasedu.nl>
2025-12-09 15:47:40 +0100

    added styling so i can see

--- resources/views/account.blade.php
+++ resources/views/account.blade.php
@@ -4,28 +4,31 @@
 
     <!-- div to focus form center of screen -->
     <div class="flex flex-col items-center justify-center min-h-screen px-4">
+        <section aria-labelledby="Login formulier" class="bg-white/80 dark:bg-gray-900/80 p-8 rounded-xl shadow-lg w-full max-w-md text-center">
 
+            <!-- page header -->
+            <header class="text-center">
+                <x-header-h1 id="login-heading" class="text-4xl font-bold mb-4 font-heading text-black dark:text-white">
+                    Account pagina
+                </x-header-h1>
 
-        <!-- page header -->
-        <header class="text-center">
-            <x-header-h1 id="login-heading" class="text-4xl font-bold mb-4 font-heading text-black dark:text-white">
-                Account pagina
-            </x-header-h1>
+                <p class="font-text text-lg mb-6 text-gray-800 dark:text-gray-300">
+                    Welkom user X
+                </p>
+            </header>
 
-            <p class="font-text text-lg mb-6 text-gray-800 dark:text-gray-300">
-                Welkom user X
-            </p>
-        </header>
+            <!-- session Status -->
+            <x-auth-session-status class="mb-4" :status="session('status')" />
 
-        <!-- session Status -->
-        <x-auth-session-status class="mb-4" :status="session('status')" />
+            <form method="POST" action=" {{route('logout') }}">
+                @csrf
+                <button type="submit" class="btn btn-primary text-gray-400">
+                    logout
+                </button>
+            </form>
 
-        <form method="POST" action=" {{route('logout') }}">
-            @csrf
-            <button type="submit" class="btn btn-primary text-gray-400">
-                logout
-            </button>
-        </form>
+        </section>
     </div>
+
 
 </x-app-layout>
