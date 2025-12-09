af0c37ab0b5d3c2ae515a7bbde43985dca153536
Martijn <m.van.der.willigen@sintlucasedu.nl>
2025-12-09 13:37:19 +0100

    remember text mad clearer, checkbox vocal feedback already worked.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -48,13 +48,15 @@
                     <x-input-error :messages="$errors->get('password')" class="mt-2" />
                 </div>
 
-                <!-- remember me -->
+
                 <div class="flex items-center justify-between mb-4">
+                    <!-- remember me -->
                     <label for="remember_me" class="inline-flex items-center">
                         <input id="remember_me" type="checkbox" class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
-                        <span class="ml-2 text-sm text-gray-600 dark:text-gray-400">Onthoud mij</span>
+                        <span class="ml-2 text-sm text-gray-600 dark:text-gray-400">Onthoud mijn gegevens</span>
                     </label>
 
+                    <!-- reset password -->
                     @if (Route::has('password.request'))
                         <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-white" href="{{ route('password.request') }}">
                            Wachtwoord vergeten?
